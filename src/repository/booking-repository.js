0bd28f57abe5b0252e7b08466d54accc2bcb3ed3
Rudra P <pandyarudra2@gmail.com>
2026-02-20 15:32:15 +0530

    added update-booking and axios req to flights-service

--- src/repository/booking-repository.js
+++ src/repository/booking-repository.js
@@ -17,6 +17,22 @@ class BookinRepository {
                  StatusCodes.INTERNAL_SERVER_ERROR);
         }
     }
+
+    async update(bookingId, data){
+        try {
+            const booking = await Booking.findbyPk(bookingId);
+            if(data.status) {
+                booking.status = data.status;
+            }
+            await booking.save();
+            return booking;
+        } catch(error) {
+            throw new AppError('RepositoryError',
+                 'Falied to update booking',
+                'There was some issue updating the booking, please try again later',
+                 StatusCodes.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
 
-module.exports = BookinRepository;
+module.exports = BookingRepository;
